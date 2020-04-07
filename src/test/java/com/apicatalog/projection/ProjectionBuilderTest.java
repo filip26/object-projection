12078361bdb41dc6da52f1dc057ff3a75246c187
Filip <filip26@gmail.com>
2020-04-08 00:16:45 +0200

    +3 new tests - ProjectionBuilderTest

--- src/test/java/com/apicatalog/projection/ProjectionBuilderTest.java
+++ src/test/java/com/apicatalog/projection/ProjectionBuilderTest.java
@@ -24,7 +24,7 @@ public class ProjectionBuilderTest {
 
 
 	@Test
-	public void test1() throws ProjectionError {
+	public void test1c() throws ProjectionError {
 		final Projection<SimpleObjectTo> projection = 
 				ProjectionBuilder
 					.bind(SimpleObjectTo.class)
@@ -44,11 +44,34 @@ public class ProjectionBuilderTest {
 		
 		Assert.assertNotNull(to);;
 		Assert.assertEquals(object1.i1, to.i1);
-		Assert.assertEquals(object1.s1, to.s1);
+		Assert.assertEquals(object1.s1, to.s1);		
 	}
 
 	@Test
-	public void test2() throws ProjectionError {
+	public void test1e() throws ProjectionError {
+		final Projection<SimpleObjectTo> projection = 
+				ProjectionBuilder
+					.bind(SimpleObjectTo.class)
+					
+					.map("i1").source(SimpleObject.class)
+					.map("s1").source(SimpleObject.class)
+					
+					.build(ProjectionRegistry.newInstance(), new TypeAdapters());
+		
+		Assert.assertNotNull(projection);
+
+		SimpleObjectTo to = new SimpleObjectTo();
+		to.i1 = 443546356;
+		to.s1 = "string-1";
+				
+		SimpleObject object1 = projection.extract(to, SimpleObject.class);
+		
+		Assert.assertNotNull(object1);
+		Assert.assertEquals(to.i1, object1.i1);
+		Assert.assertEquals(to.s1, object1.s1);
+	}
+	@Test
+	public void test2c() throws ProjectionError {
 		final Projection<SimpleObjectTo> projection = 
 				ProjectionBuilder
 					.bind(SimpleObjectTo.class)
@@ -69,6 +92,30 @@ public class ProjectionBuilderTest {
 		Assert.assertNotNull(to);;
 		Assert.assertEquals(Integer.valueOf(object1.s1), to.i1);
 		Assert.assertEquals(String.valueOf(object1.i1), to.s1);
+	}
+
+	@Test
+	public void test2e() throws ProjectionError {
+		final Projection<SimpleObjectTo> projection = 
+				ProjectionBuilder
+					.bind(SimpleObjectTo.class)
+					
+					.map("i1").source(SimpleObject.class, "s1")
+					.map("s1").source(SimpleObject.class, "i1")
+					
+					.build(ProjectionRegistry.newInstance(), new TypeAdapters());
+		
+		Assert.assertNotNull(projection);
+
+		SimpleObjectTo to = new SimpleObjectTo();
+		to.i1 = 443546356;
+		to.s1 = "57566735";
+		
+		SimpleObject object1 = projection.extract(to, SimpleObject.class);
+		
+		Assert.assertNotNull(object1);
+		Assert.assertEquals(Integer.valueOf(to.s1), object1.i1);
+		Assert.assertEquals(String.valueOf(to.i1), object1.s1);
 	}
 
 	@Test
@@ -126,7 +173,7 @@ public class ProjectionBuilderTest {
 	}
 
 	@Test
-	public void test51() throws ProjectionError {
+	public void test5c1() throws ProjectionError {
 		
 		ProjectionRegistry registry = ProjectionRegistry.newInstance();
 		
@@ -168,7 +215,48 @@ public class ProjectionBuilderTest {
 	}
 
 	@Test
-	public void test52() throws ProjectionError {
+	public void test5e1() throws ProjectionError {
+		
+		ProjectionRegistry registry = ProjectionRegistry.newInstance();
+		
+		Assert.assertNotNull( 
+				ProjectionBuilder
+					.bind(Object1To.class)
+					
+					.map("object2", true).source(Object1.class).optional()
+
+					.map("id").source(Object1.class)
+					
+					.build(registry, new TypeAdapters())
+					);
+
+		Assert.assertNotNull(
+				ProjectionBuilder
+					.bind(Object2To.class)
+					
+					.map("id").source(Object2.class)
+					
+					.build(registry, new TypeAdapters())
+					);
+		
+		Object1To to1 = new Object1To();
+		to1.id = "AREW2324E";
+		
+		Object2To to2 = new Object2To();
+		to2.id = "3GFD42E";
+		
+		to1.object2 = to2;
+		
+		Object1 object1 = registry.extract(to1, Object1.class);
+		
+		Assert.assertNotNull(object1);;
+		Assert.assertEquals(to1.id, object1.id);
+		
+		Assert.assertNotNull(object1.object2);
+		Assert.assertEquals(to2.id, object1.object2.id);		
+	}
+	@Test
+	public void test5c2() throws ProjectionError {
 		
 		ProjectionRegistry registry = ProjectionRegistry.newInstance();
 
